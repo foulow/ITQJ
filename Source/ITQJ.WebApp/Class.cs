47592e9882b4741612ecb26959d6e5fe956f6f48
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2020-07-11 19:37:54 -0400

    front end,  diseñando el home

---
+++ Source/ITQJ.WebApp/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITQJ.WebApp
+{
+    public class Class
+    {
+        [Required]
+        [StringLength(25)]
+        public string User { get; set; }
+            
+        [Required]
+        [StringLength(25)]
+        public string password { get; set; }
+    }
+}
--- Source/ITQJ.WebPWA/Program.cs
+++ Source/ITQJ.WebPWA/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using BlazorStrap;
 
-namespace ITQJ.WebApp
+namespace ITQJ.WebPWA
 {
     public class Program
     {
