304ba336c0e638417ed0c608294b74c68c0766c6
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2020-07-12 11:32:18 -0400

    modelos

---
+++ Source/ITQJ.WebPWA/Pages/LoginRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITQJ.WebPWA.Pages
+{
+    public class LoginRequest
+    {
+        [Required]
+        [StringLength(25)]
+        public string user { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string password { get; set; }
+    }
+}
