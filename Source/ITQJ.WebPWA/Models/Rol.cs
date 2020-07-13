958d248498e454abf7cd7baf0525bb6649192995
Jeffrey Jose <jeffreyissaul@hotmail.com>
2020-07-12 21:44:31 -0400

    hotfix a vistas del proyecto ITQJ.WebPWA, se incluyo los modelos de Rol y User para el proyecto ITQj.WebPWA.

---
+++ Source/ITQJ.WebPWA/Models/Rol.cs
@@ -0,0 +1,10 @@
+﻿namespace ITQJ.WebPWA.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+    }
+}
---
+++ Source/ITQJ.WebPWA/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace ITQJ.WebPWA.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public int RolId { get; set; }
+
+        public virtual Rol Rol { get; set; }
+    }
+}
