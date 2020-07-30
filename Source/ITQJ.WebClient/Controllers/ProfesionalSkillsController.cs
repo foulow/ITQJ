866df368a531b95b9844a4fbfa3ac15f47b121b0
LuisEduardoFrias <54819225+LuisEduardoFrias@users.noreply.github.com>
2020-07-30 19:56:18 -0400

    add controlller and view ProfesionalSkills

---
+++ Source/ITQJ.WebClient/Controllers/ProfesionalSkillsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ITQJ.Domain.DTOs;
+
+namespace ITQJ.WebClient.Controllers
+{
+    public class ProfesionalSkillsController : BaseController
+    {
+        public ProfesionalSkillsController(IServiceProvider serviceProvider) : base(serviceProvider) { }
+
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> EdictSkills()
+        {
+            var profesionalSkills = await CallApiGETAsync<ProfesionalSkillResponseDTO>("/api/ProfesionalSkills/" + userName);
+
+            if (profesionalSkills == null)
+                return PageNotFound();
+
+            return View(profesionalSkills);
+        }
+
+
+    }
+}
